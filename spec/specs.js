f9ec72c752cba367110fe4854e24326bf16cc363
Paul Ogawa <paul.ogawa7@gmail.com>
2015-06-24 14:03:12 -0700

    Add tests

--- spec/specs.js
+++ spec/specs.js
@@ -7,17 +7,4 @@ describe("Pizza", function() {
 
   });
 
-  it("can have toppings added to it", function() {
-    var pizza = new Pizza("large")
-    pizza.addTopping("pepperoni")
-    expect(pizza.toppings).to.eql(["pepperoni"])
-  });
-
-  it("can have its price calculated", function() {
-    var pizza = new Pizza("large")
-    pizza.addTopping("pepperoni")
-    pizza.addTopping("mushrooms")
-    pizza.calculateCost()
-    expect(pizza.cost).to.equal(15)
-  });
 });
